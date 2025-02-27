e7e87d68762ad74e7c10032ad960953efb36322e
yohnalcala26@gmail.com <sistemas06@chavacal.local>
2025-02-27 14:03:50 -0400

    Agregando estilos y funcionalidades

--- title.js
+++ title.js
@@ -1,3 +1,7 @@
 const title = document.querySelector('.button');
+const container = document.querySelector('.container');
 
-title.addEventListener('click', ()=> alert("Diste click"));
+title.addEventListener('click', function(){
+    container.style.backgroundColor = "#282C34";
+    container.style.color = "#fff";
+});
